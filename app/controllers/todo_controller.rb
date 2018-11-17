cd34f79bfcfce8c90948bf9ec1b130be22ddf52d
Yisam Laracuente <jocktiger@gmail.com>
2018-11-17 20:06:12 +0000

    ruby ver4

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,8 +1,31 @@
 class TodoController < ApplicationController
     def index 
     end
-    def show 
-        @todo_description = "todolist"
+    def show
         @todo_pomodoro_estimate = "900"
+        todo_id = params[:id] 
+        if todo_id == '1'
+            @todo_description = "shop"
+            @todo_pomodoro_estimate = "5"
+        elsif todo_id == '2'    
+            @todo_description = "walk"  
+            @todo_pomodoro_estimate = "4"
+        elsif todo_id == '3'
+            @todo_description = "school"
+            @todo_pomodoro_estimate = "6"
+        elsif todo_id == '4'
+            @todo_description = "work"
+            @todo_pomodoro_estimate = "5"
+        elsif todo_id == '5'
+            @todo_description = "play"
+            @todo_pomodoro_estimate = "3"
+        elsif todo_id == '6'
+            @todo_description = "fallout76"
+            @todo_pomodoro_estimate = "90909"
+        elsif todo_id == '7'
+            @todo_description = "fallout76day2"
+            @todo_pomodoro_estimate = "900000"
+        
+        end
     end
 end
