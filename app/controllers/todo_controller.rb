426741abaa46dc82619ade5e11a6582efb5a5fb5
Yisam Laracuente <jocktiger@gmail.com>
2018-11-17 16:50:52 +0000

    ruby ver3

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,8 @@
 class TodoController < ApplicationController
     def index 
-   end
+    end
+    def show 
+        @todo_description = "todolist"
+        @todo_pomodoro_estimate = "900"
+    end
 end
